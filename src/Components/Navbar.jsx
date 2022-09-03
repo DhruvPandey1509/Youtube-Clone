d6bf5c9b753cda2cf33c752dd30b5785eafb2eb5
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-09-03 19:49:57 +0530

    Added Search bar and Nav Bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom';
+// import { logo } from '../Utils/Constants'
+import SearchBar from './SearchBar';
+import logo from '../Utils/Youtube-logo.jpg'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack
+      direction="row"
+      alignItems="centre"
+      p={2}
+      sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+    >
+      <Link to="/" style={{ display: 'flex', alignItems: 'centre' }}>
+        <img src={logo} alt="youtube-logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
   )
 }
 
