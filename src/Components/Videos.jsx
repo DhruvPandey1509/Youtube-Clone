5b4473ca7f1f6811232a622fd35e9bcbeddf469c
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-09-03 23:04:01 +0530

    Api Fetch and Sidebar functionality

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
+import { Stack, Box } from '@mui/material'
+import { VideoCard, ChannelCard } from './index'
+const Videos = ({ videos }) => {
+    console.log(videos)
+    return (
+        <Stack direction="row" flewWrap="wrap" justifyContent="start" gap={2}>
+            {videos.map((item, idx) => (
+                <Box key={idx}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+                </Box>
+            ))}
+        </Stack>
+    )
 }
 
 export default Videos
